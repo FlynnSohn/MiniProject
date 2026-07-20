3990244b6472d6d537a63eef4b0f6d66b0c1556a
FlynnSohn <137607802+FlynnSohn@users.noreply.github.com>
2026-07-20 16:29:57 +0900

    asset: 에셋에 체력 감소 효과 적용

--- Assets/01_Scripts/Battle/DefendCalculator.cs
+++ Assets/01_Scripts/Battle/DefendCalculator.cs
@@ -5,8 +5,6 @@ public static class DefendCalculator
 {
     public static int Calculate(Character receiver, int baseDefend)
     {
-        // 난 모르겠네... 방어를 타겟이 받을 일이 있나? 그런 카드도 없고 몬스터 스킬도 없고
-        // scope도 안 나눴고... 
         // 방어카드를 낼 때 반영될 건 민첩뿐인듯
         // 그래도 일단 둘다 더해두긴 할까
         int sum = receiver.SumDefendGainedBonus();
